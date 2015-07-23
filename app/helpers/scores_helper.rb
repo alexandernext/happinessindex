16beacdd4aab35702d34f95122d4a38a34f56bda
alexander95015 <alexander95015@gmail.com>
2015-07-23 12:20:12 +0800

    monthly indicator

--- app/helpers/scores_helper.rb
+++ app/helpers/scores_helper.rb
@@ -7,6 +7,7 @@ module ScoresHelper
       return week_scores.sum("value").to_f / week_scores.count
     end
   end
+
   def daily_average
     daily_scores = current_user.scores.where(day: Date.today)
     if daily_scores.count == 0
@@ -15,4 +16,14 @@ module ScoresHelper
       return daily_scores.sum("value").to_f / daily_scores.count
     end
   end
+
+  def monthly_average
+    monthly_scores = current_user.scores.where("day >= ?", Date.new(Date.today.year, Date.today.month, 1))
+    if monthly_scores.count == 0
+      return nil
+    else
+      return monthly_scores.sum("value").to_f / monthly_scores.count
+    end
+  end
+
 end
